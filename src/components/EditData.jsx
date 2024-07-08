fa5f88a0fc8f467c7c72c1cddcf3f28a3fe15843
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-07-08 10:06:08 +0000

    baru display

--- src/components/EditData.jsx
+++ src/components/EditData.jsx
@@ -1,28 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Button, Form, Modal, Row, Stack } from "react-bootstrap";
+import InputMACAddress from "./InputMACAddress";
 
 function EditData({ show, onHide }) {
-  const [valueBlockMAC, setValueBlockMAC] = useState([
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
-
-  const [valueMAC, setValueMAC] = useState("")
-
-  const handleChangeBlockMAC = (index, e) => {
-    setValueBlockMAC(
-      valueBlockMAC.map((item, i) => (i === index ? e.target.value : item))
-    );
-  };
-
-  useEffect(() => {
-    setValueMAC(valueBlockMAC.join(":"))
-  }, [valueBlockMAC])
-
   return (
     <>
       <Modal show={show} onHide={onHide}>
@@ -35,31 +15,7 @@ function EditData({ show, onHide }) {
               <Form.Label>Nama Pengguna</Form.Label>
               <Form.Control type="text" placeholder="Nama..." />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Alamat MAC Wifi</Form.Label>
-              <Stack direction="horizontal" gap={2} className="my-2">
-                {valueBlockMAC.map((d, i) => (
-                  <>
-                    <Form.Control
-                      name={`value${i}`}
-                      type="text"
-                      className="text-center"
-                      maxLength="2"
-                      value={valueBlockMAC[i]}
-                      onChange={(e) => handleChangeBlockMAC(i, e)}
-                    />
-                    {i < 5 && <span>:</span>}
-                  </>
-                ))}
-              </Stack>
-              <Form.Control
-                type="text"
-                placeholder="Nama..."
-                value={valueMAC}
-                // onChange={handleChangeInputMAC}
-                disabled
-              />
-            </Form.Group>
+            <InputMACAddress />
           </Form>
         </Modal.Body>
         <Modal.Footer>
