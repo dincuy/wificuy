d84829ed6f153714b8a1c7bbe3d032f539017b77
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-07-09 16:24:03 +0000

    hoaam

--- src/components/TableCustomer.jsx
+++ src/components/TableCustomer.jsx
@@ -85,7 +85,7 @@ function TableCustomer() {
   const handleSaveEdit = (e) => {
     e.preventDefault();
     axios
-      .put(`${import.meta.env.API_URL}/api/wifi/${dataForEdit._id}`, dataForEdit)
+      .put(`${process.env.API_URL}api/wifi/${dataForEdit._id}`, dataForEdit)
       .then((response) => {
         setDataCustomers(
           dataCustomers.map((item) =>
@@ -109,7 +109,7 @@ function TableCustomer() {
   // Hapus pelanggan wifi
   const handleDeleteOK = () => {
     axios
-      .delete(`${import.meta.env.API_URL}/api/wifi/${dataForEdit._id}`)
+      .delete(`${process.env.API_URL}api/wifi/${dataForEdit._id}`)
       .then((response) => {
         setDataCustomers(
           dataCustomers.filter((item) => item._id !== dataForEdit._id)
@@ -144,7 +144,7 @@ function TableCustomer() {
 
   useEffect(() => {
     axios
-      .get(`${import.meta.env.API_URL}/api/wifi`)
+      .get(`${process.env.API_URL}api/wifi`)
       .then((response) => {
         setDataCustomers(response.data);
         // setLoading(false);
